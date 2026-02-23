5703459be52eff4136e11df0464c90ab0fc525d1
sovannra_kong <sovannra_kong@phillipbank.com.kh>
2026-02-23 14:01:04 +0700

    Migrate to SPM

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.7
+import PackageDescription
+
+let package = Package(
+    name: "BakongKHQR",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "BakongKHQR",
+            targets: ["BakongKHQR"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "BakongKHQR",
+            path: "BakongKHQR/Classes",
+            publicHeadersPath: "Public"
+        ),
+    ]
+)
